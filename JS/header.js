8524f5819964576c27799e4a114db9fb430dc1fc
blossomkaler <54261097+blossomkaler@users.noreply.github.com>
2023-04-03 14:29:59 +0530

    mobile-design

--- JS/header.js
+++ JS/header.js
@@ -1,17 +1,13 @@
 const hamburger = document.querySelector('.hamburger');
-const header = document.querySelector('.header');
 const logo = document.querySelector('.logo');
 const primaryNav = document.querySelector('.primary-nav');
-const secondaryNav = document.querySelector('.secondary-nav');
 const navItems = document.querySelector('.nav-items');
 const cross = document.querySelector('.close-menu');
 
 hamburger.addEventListener('click',function(){
     hamburger.classList.add('active');
-    header.classList.add('active');
     logo.classList.add('active');
     primaryNav.classList.add('active');
-    secondaryNav.classList.add('active');
     navItems.classList.add('active');
     cross.classList.add('active');
     
@@ -20,10 +16,8 @@ hamburger.addEventListener('click',function(){
 
 cross.addEventListener('click',function(){
     hamburger.classList.remove('active');
-    header.classList.remove('active');
     logo.classList.remove('active');
     primaryNav.classList.remove('active');
-    secondaryNav.classList.remove('active');
     navItems.classList.remove('active');
     cross.classList.remove('active');
     
